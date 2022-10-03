d6e822112c7cfbe5ee1c2a2654ddf6bbe859c4b3
imjohnnyy <ldong4056@gmail.com>
2022-10-03 19:56:31 +1300

    Changed article form and added styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from "react-router-dom";
+import "./index.css";
+import SubmitArticle from "./pages/SubmitArticle";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <div>
+        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        <ul className="header">
+          <li>
+            <NavLink exact to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SEPractice" >Select the Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SubmitArticle" >Submit an Article</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<SubmitArticle />} />
+
+
+            <Route path="/*" element={<Navigate to="404" replace />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
