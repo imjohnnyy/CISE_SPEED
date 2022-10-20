1db242b7c477bca31d3f8422c6734af515803c95
Aman Perhar <amanperhar10@gmail.com>
2022-10-20 17:09:27 +1300

    Changed to public

--- server.js
+++ server.js
@@ -42,10 +42,10 @@ const PORT = process.env.PORT || 5000;
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+app.use(express.static(path.resolve(__dirname, "./frontend/public")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./frontend/public", "index.html"));
 });
 
 const port = process.env.PORT || 8082;
